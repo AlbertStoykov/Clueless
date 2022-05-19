a853b5c969156ddef7572beb39a546fa10a05c1e
THAYAANS <thayaan.s@gmail.com>
2022-05-19 10:57:53 +0100

    landing page

--- client/src/Pages/LandingPage/Index.js
+++ client/src/Pages/LandingPage/Index.js
@@ -1,37 +1,35 @@
 import React from 'react';
-import {
-  Typography,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  CssBaseline,
-  Grid,
-  Toolbar,
-  Container,
-  AppBar,
-} from '@mui/material';
+import { Typography, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, AppBar, createTheme, Button } from '@mui/material';
 import { Box, height } from '@mui/system';
+import { ThemeProvider } from '@emotion/react';
 
 const LandingPage = () => {
+
+  const theme = createTheme({
+    typography: {
+
+      fontFamily: 'Luckiest Guy',
+    },
+
+  })
+
   return (
     <>
       {/* <CssBaseline /> */}
-      <AppBar
-        sx={{
-          position: 'absolute',
-          background: 'purple',
-        }}
-      >
-        <Toolbar>
-          <Typography variant="h6" color="white">
-            Clueless
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        <div>
-          <Container sx={{ padding: '80px' }}>
+      <ThemeProvider theme={theme}>
+        <AppBar sx={{
+          position: "absolute",
+          background: 'purple'
+        }}>
+          <Toolbar>
+            <Typography variant="h6" color="white" align='center'>Clueless</Typography>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+
+      <main >
+        <div >
+          <Container sx={{ padding: "80px" }}>
             <Box
               component="img"
               sx={{
@@ -44,59 +42,38 @@ const LandingPage = () => {
               src="assets/images/clueless.png"
             />
 
-            <Box
-              sx={{
-                backgroundImage: `url(${'assets/images/jason-leung.jpg'})`,
-                position: 'absolute',
-                right: '0px',
-                left: '0px',
-                height: '400px',
-                width: '100vw',
-              }}
+
+            <Box sx={{
+              backgroundImage: `url(${"assets/images/jason-leung.jpg"})`,
+              position: "absolute",
+              right: "0px",
+              left: "0px",
+              height: "400px",
+              width: "100vw"
+            }}
             >
-              <Typography
-                fontFamily="Press Start 2P"
-                variant="h4"
-                align="center"
-                color="white"
-                paragraph
-                padding={'50px'}
-              >
-                Game rules
-              </Typography>
-              <Typography
-                variant="h6"
-                align="center"
-                color="white"
-                paragraph
-                padding={'10px'}
-              >
-                1. No cheating
-              </Typography>
-              <Typography
-                variant="h6"
-                align="center"
-                color="white"
-                paragraph
-                padding={'10px'}
-              >
-                2. Loser Drinks
-              </Typography>
-              <Typography
-                variant="h6"
-                align="center"
-                color="white"
-                paragraph
-                padding={'10px'}
-              >
-                3. Answer within the timer
-              </Typography>
+              <ThemeProvider theme={theme}>
+                <Typography variant='h4' align="center" color="white" paragraph padding={"50px"}>Game rules</Typography>
+                <Typography variant='h6' align="center" color="white" paragraph padding={"10px"}>1. No cheating</Typography>
+                <Typography variant='h6' align="center" color="white" paragraph padding={"10px"}>2. Loser Drinks</Typography>
+                <Typography variant='h6' align="center" color="white" paragraph padding={"10px"}>3. Answer within the timer</Typography>
+              </ThemeProvider>
+
             </Box>
+            <Box sx={{
+              padding: "500px",
+            }}>
+              <Button sx={{
+                width: "100px",
+              }}>Let The Games Begin</Button>
+            </Box>
+
           </Container>
         </div>
       </main>
     </>
-  );
+  )
+
 };
 
 export default LandingPage;
