137f8aec966e10b5f2a893cdc31ff729dbe76f40
AlbertStoykov <albert.stoykov@gmail.com>
2022-05-19 12:52:29 +0100

    github fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import {
   QuizSetup,
   Questions,
   FinalScore,
   NotFoundPage,
   LandingPage,
-  GameRoom,
 } from "./pages";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/gameroom" element={<GameRoom />} />
       <Route path="/getquiz" element={<QuizSetup />} />
       <Route path="/questions" element={<Questions />} />
       <Route path="/score" element={<FinalScore />} />
--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -2,5 +2,4 @@ export { default as FinalScore } from "./FinalScore";
 export { default as QuizSetup } from "./QuizSetup";
 export { default as Questions } from "./Questions";
 export { default as LandingPage } from "./LandingPage";
-export { default as GameRoom } from "./GameRoom";
 export { default as NotFoundPage } from "./NotFoundPage";
