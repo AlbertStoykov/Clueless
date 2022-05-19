757e6758fb299909e7a7e117ef7250b792a19f0b
“iAmash412” <aafthab.ashraff@gmail.com>
2022-05-19 16:39:49 +0100

    Latest Layout page

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Typography,
   Card,
@@ -12,28 +12,62 @@ import {
   AppBar,
   createTheme,
   Button,
-} from "@mui/material";
-import { Box } from "@mui/system";
+} from '@mui/material';
+import { Box, color, height } from '@mui/system';
+import { ThemeProvider } from '@emotion/react';
+import { pink } from '@mui/material/colors';
+import { deepOrange } from '@mui/material/colors';
+import { shadows } from '@mui/system';
 
 const LandingPage = () => {
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Luckiest Guy',
+    },
+  });
+
+  const theme2 = createTheme({
+    typography: {
+      fontFamily: 'Luckiest Guy',
+    },
+    palette: {
+      primary: {
+        main: pink[400],
+      },
+    },
+  });
+
+  const theme3 = createTheme({
+    background: {
+      palette: {
+        primary: {
+          main: deepOrange[400],
+        },
+      },
+    },
+  });
+
   return (
-    <Box role="landing-page">
+    <>
       {/* <CssBaseline /> */}
-      <AppBar
-        sx={{
-          position: "absolute",
-          background: "purple",
-        }}
-      >
-        <Toolbar>
-          <Typography variant="h6" color="white">
-            Clueless
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <ThemeProvider theme={theme}>
+        <AppBar
+          sx={{
+            position: 'absolute',
+            background: 'purple',
+          }}
+        >
+          <Toolbar>
+            <Typography variant="h6" color="white" align="centre">
+              Clueless
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+
       <main>
         <div>
-          <Container sx={{ padding: "80px" }}>
+          <Container sx={{ padding: '80px' }}>
             <Box
               component="img"
               sx={{
@@ -48,56 +82,84 @@ const LandingPage = () => {
 
             <Box
               sx={{
-                backgroundImage: `url(${"assets/images/jason-leung.jpg"})`,
-                position: "absolute",
-                right: "0px",
-                left: "0px",
-                height: "400px",
-                width: "100vw",
+                backgroundImage: `url(${'assets/images/jason-leung.jpg'})`,
+                position: 'absolute',
+                right: '0px',
+                left: '0px',
+                height: '450px',
+                width: '100vw',
               }}
             >
-              <Typography
-                fontFamily="Press Start 2P"
-                variant="h4"
-                align="center"
-                color="white"
-                paragraph
-                padding={"50px"}
-              >
-                Game rules
-              </Typography>
-              <Typography
-                variant="h6"
-                align="center"
-                color="white"
-                paragraph
-                padding={"10px"}
-              >
-                1. No cheating
-              </Typography>
-              <Typography
-                variant="h6"
-                align="center"
-                color="white"
-                paragraph
-                padding={"10px"}
-              >
-                2. Loser Drinks
-              </Typography>
-              <Typography
-                variant="h6"
-                align="center"
-                color="white"
-                paragraph
-                padding={"10px"}
-              >
-                3. Answer within the timer
-              </Typography>
+              <ThemeProvider theme={theme}>
+                <Typography
+                  variant="h4"
+                  align="center"
+                  color="white"
+                  paragraph
+                  padding={'20px'}
+                >
+                  Game rules
+                </Typography>
+                <Typography
+                  variant="h6"
+                  align="center"
+                  color="white"
+                  paragraph
+                  padding={'10px'}
+                >
+                  1. No cheating
+                </Typography>
+                <Typography
+                  variant="h6"
+                  align="center"
+                  color="white"
+                  paragraph
+                  padding={'10px'}
+                >
+                  2. Loser Drinks
+                </Typography>
+                <Typography
+                  variant="h6"
+                  align="center"
+                  color="white"
+                  paragraph
+                  padding={'10px'}
+                >
+                  3. Answer within the timer
+                </Typography>
+              </ThemeProvider>
+
+              <Box>
+                <ThemeProvider theme={theme2}>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      width: '200px',
+                      boxShadow: 10,
+                    }}
+                  >
+                    <Typography> Let The Games Begin</Typography>
+                  </Button>
+                </ThemeProvider>
+              </Box>
+
+              <Box
+                sx={{
+                  mt: '90px',
+                  background: deepOrange[500],
+                  position: 'absolute',
+                  right: '0px',
+                  left: '0px',
+                  height: '60px',
+                  width: '100vw',
+                }}
+                gutterBottom
+              ></Box>
             </Box>
           </Container>
         </div>
       </main>
-    </Box>
+    </>
   );
 };
 
